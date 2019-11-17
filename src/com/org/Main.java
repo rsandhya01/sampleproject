17b008c2e2a5269e4c901941610c5c45a81a7cb0
rsandhya01 <ralla.sandhya594@gmal.com>
2019-11-17 16:02:45 +0530

    add sample class and call sample m1 method in main class

--- src/com/org/Main.java
+++ src/com/org/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("hello");
+	Sample sample=new Sample();
+	for(int i=0;i<5;i++){
+	    sample.m1();
+    }
     }
 }
---
+++ src/com/org/Sample.java
@@ -0,0 +1,7 @@
+package com.org;
+
+public class Sample {
+    public void m1(){
+        System.out.println("sample method is created");
+    }
+}
