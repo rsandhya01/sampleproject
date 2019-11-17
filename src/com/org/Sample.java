7eec51586033ab3db224068890c1694a654578d1
rsandhya01 <42083009+rsandhya01@users.noreply.github.com>
2019-11-17 16:09:16 +0530

    done changes in sample file

--- src/com/org/Sample.java
+++ src/com/org/Sample.java
@@ -3,5 +3,7 @@ package com.org;
 public class Sample {
     public void m1(){
         System.out.println("sample method is created");
+        System.out.println("hello did changes in repository");
+        
     }
 }
